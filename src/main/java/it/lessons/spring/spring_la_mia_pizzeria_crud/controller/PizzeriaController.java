bec8886fc60db626c65c104114c42520cb22de30
MarcoGreco74 <marcogreco.gibson@gmail.com>
2025-04-04 23:42:32 +0200

    feat: add show task

--- src/main/java/it/lessons/spring/spring_la_mia_pizzeria_crud/controller/PizzeriaController.java
+++ src/main/java/it/lessons/spring/spring_la_mia_pizzeria_crud/controller/PizzeriaController.java
@@ -1,15 +1,18 @@
 package it.lessons.spring.spring_la_mia_pizzeria_crud.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import it.lessons.spring.spring_la_mia_pizzeria_crud.model.Pizzeria;
 import it.lessons.spring.spring_la_mia_pizzeria_crud.repository.PizzeriaRepository;
+
 
 @Controller
 @RequestMapping("/pizzeria")
@@ -23,6 +26,17 @@ public class PizzeriaController {
         List<Pizzeria> result = pizzeriaRepository.findAll();
         model.addAttribute("list", result);
         return "pizzeria/index";
+    }
+
+    @GetMapping("/show/{id}")
+    public String show(@PathVariable("id") Integer id, Model model) {
+        Optional<Pizzeria> optPizzeria = pizzeriaRepository.findById(id);
+        if(optPizzeria.isPresent()){
+            model.addAttribute("pizza", optPizzeria.get());
+            return "pizzeria/show";
+        }
+        model.addAttribute("errorMsg", "Pagina non trovata");
+        return "pizzeria/error_page";
     }
     
 }
