9b6c94824ffd1173c83671b8aceb48a739eb2979
MarcoGreco74 <marcogreco.gibson@gmail.com>
2025-04-06 23:14:12 +0200

    feat: add show + form + refactoring

--- src/main/java/it/lessons/spring/spring_la_mia_pizzeria_crud/controller/PizzeriaController.java
+++ src/main/java/it/lessons/spring/spring_la_mia_pizzeria_crud/controller/PizzeriaController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import it.lessons.spring.spring_la_mia_pizzeria_crud.model.Pizzeria;
 import it.lessons.spring.spring_la_mia_pizzeria_crud.repository.PizzeriaRepository;
@@ -21,22 +22,53 @@ public class PizzeriaController {
     @Autowired
     private PizzeriaRepository pizzeriaRepository;
 
-    @GetMapping
+    /*@GetMapping
     public String index(Model model) {
         List<Pizzeria> result = pizzeriaRepository.findAll();
         model.addAttribute("list", result);
         return "pizzeria/index";
+    }*/
+
+    @GetMapping
+    public String index(Model model, @RequestParam(name="keyword", required=false) String nome){
+        List<Pizzeria> result;
+        if(nome != null  && !nome.isBlank()){
+            //result = pizzeriaRepository.findByNome(nome);
+            result = pizzeriaRepository.findByNomeContainingIgnoreCase(nome);
+        }else{
+            result = pizzeriaRepository.findAll();
+        }
+        model.addAttribute("list", result);
+        return "pizzeria/index";
     }
 
-    @GetMapping("/show/{id}")
-    public String show(@PathVariable("id") Integer id, Model model) {
+    @GetMapping("/show_id/{id}")
+    public String show_id(@PathVariable("id") Integer id, Model model) {
         Optional<Pizzeria> optPizzeria = pizzeriaRepository.findById(id);
         if(optPizzeria.isPresent()){
             model.addAttribute("pizza", optPizzeria.get());
-            return "pizzeria/show";
+            return "pizzeria/show_id";
         }
         model.addAttribute("errorMsg", "Pagina non trovata");
         return "pizzeria/error_page";
     }
-    
+
+    /*@GetMapping("/show_name")
+   public String showname(Model model, @RequestParam(name="keyword", required=false) String nome) {
+      List<Pizzeria> result;
+      if(nome != null && !nome.isBlank()) {
+         result = pizzeriaRepository.findByNome(nome);
+         //result = pizzeriaRepository.findByNomeContainingIgnoreCase(nome);
+         //model.addAttribute("list", result);
+      } else {
+         //result = pizzeriaRepository.findAll();
+           model.addAttribute("errorMsg", "Pagina non trovata");
+           return "pizzeria/error_page";
+      }
+        model.addAttribute("list", result);
+        return "pizzeria/showname";
+   }*/	
+  //// Problemi ////
+   // come creare una nuova route, diversa da index che riceve i dati del form ? 
+   // perchè la query custom findByNome non funziona ?
 }
--- src/main/java/it/lessons/spring/spring_la_mia_pizzeria_crud/repository/PizzeriaRepository.java
+++ src/main/java/it/lessons/spring/spring_la_mia_pizzeria_crud/repository/PizzeriaRepository.java
@@ -1,10 +1,17 @@
 package it.lessons.spring.spring_la_mia_pizzeria_crud.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import it.lessons.spring.spring_la_mia_pizzeria_crud.model.Pizzeria;
 
 public interface PizzeriaRepository extends JpaRepository<Pizzeria, Integer>{
 
-    
+    /*@Query(value = "SELECT * FROM pizzeria WHERE nome = :nome", nativeQuery = true)
+        Optional<Pizzeria> trovaPerENome(@Param("nome") String nome);*/
+        public List<Pizzeria> findByNomeContainingIgnoreCase(String nome);
+        //@Query(value = "SELECT * FROM pizzeria WHERE nome like ?", nativeQuery = true)
+       // public List<Pizzeria> findByNome(String nome);
+
 }
